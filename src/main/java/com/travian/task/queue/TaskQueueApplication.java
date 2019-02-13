197d08fe12da66adb5f1863bbfaeee186645b589
sudipto <sudipto1306@gmail.com>
2019-02-12 23:45:39 -0600

    2

--- src/main/java/com/travian/task/queue/TaskQueueApplication.java
+++ src/main/java/com/travian/task/queue/TaskQueueApplication.java
@@ -2,13 +2,34 @@ package com.travian.task.queue;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 
 @SpringBootApplication
-public class TaskQueueApplication {
+public class TaskQueueApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TaskQueueApplication.class, args);
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(TaskQueueApplication.class);
+	}
+	
+	@Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .useDefaultResponseMessages(false)
+                .select()
+                .apis( RequestHandlerSelectors.basePackage( "com.travian.task" ) )
+                .paths(PathSelectors.any())
+                .build();
+    }
 }
-
